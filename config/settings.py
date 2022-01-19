e8bfab1a72f67beee38cc97d33011552beb6d834
Pankaj Garkoti <garkotipankaj@gmail.com>
2022-01-19 19:53:17 +0530

    heroku

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'django-insecure-t6vr)9wk#22)pc%8p8#zz91iwa8*33&y_itxtgp-y9)4ko6*b_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
